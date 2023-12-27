071045ba89ce63890315c3bdcef09a72c47d3ac6
adamsenn <32107702+adamsenn@users.noreply.github.com>
2023-12-27 14:27:55 -0500

    Generated from Spring Initilizer

---
+++ demo/src/test/java/com/adamsenn/dgs/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamsenn.dgs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
